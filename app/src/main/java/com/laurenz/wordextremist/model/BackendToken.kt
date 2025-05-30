21582d43c4fd2882444167889a9c27e7891d7fe4
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2025-05-30 22:38:23 +0200

    Added user to fetch user information from token

--- app/src/main/java/com/laurenz/wordextremist/model/BackendToken.kt
+++ app/src/main/java/com/laurenz/wordextremist/model/BackendToken.kt
@@ -5,6 +5,10 @@ import com.google.gson.annotations.SerializedName
 data class BackendToken(
     @SerializedName("access_token")
     val access_token: String,
+
     @SerializedName("token_type")
-    val token_type: String
+    val token_type: String,
+
+    @SerializedName("user") // This annotation ensures Gson maps the "user" JSON key
+    val user: UserPublic?   // Add the user field, make it nullable for safety
 )
